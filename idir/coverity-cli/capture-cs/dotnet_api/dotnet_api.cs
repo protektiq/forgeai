a3c4b614c4ae51c9df644d00efddc24188c6b59b
Carlos Almeyda <almeyda@blackduck.com>
2026-03-04 18:21:26 +0000

    committing idir and coverity config file

---
+++ idir/coverity-cli/capture-cs/dotnet_api/dotnet_api.cs
@@ -0,0 +1,13 @@
+#pragma warning disable 108, 8610
+#pragma warning disable 8617, 8714
+#pragma warning disable 626, 824
+internal unsafe class Program
+{
+    private static void <top-level-statements-entry-point>(string[] args) => throw null;
+}
+
+internal unsafe class GenerateRequest : System.IEquatable<GenerateRequest>
+{
+    public GenerateRequest(string? Prompt) => throw null;
+    public string? Prompt { get => throw null; set => throw null; }
+}
